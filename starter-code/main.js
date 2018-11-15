4acdc4e815a8a260fd78abb9ee0fe0ffa94e9a35
Craig Lymus <craiglymus@gmail.com>
2018-11-15 10:49:03 -0800

    completed array hw

--- starter-code/main.js
+++ starter-code/main.js
@@ -5,6 +5,10 @@
 
 var ages = [28, 30, 17, 14, 12, 20, 13, 36, 68, 49, 37, 55, 56, 26];
 
+for (var i = 0; i < ages.length; i++){
+	console.log(ages[i]);
+}
+
 /*
 	Part 2: Let's create a new variable, "younger", which holds an empty array.
 	Using a for-loop, populate the "younger" array with numbers from the "ages"
@@ -15,6 +19,16 @@ var ages = [28, 30, 17, 14, 12, 20, 13, 36, 68, 49, 37, 55, 56, 26];
 	Hint: Use the array.push() method to add an element to the end of an array.
 	(https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Array/push)
 */
+
+var younger = [];
+
+for (var i = 0; i < ages.length; i++){
+	if (ages[i] <= 20) {
+		younger.push(ages[i]);
+	}
+}
+
+console.log(younger);
 
 /*
 	Part 3: We're comfortable going through an element from beginning to end, but let's
@@ -28,10 +42,54 @@ var ages = [28, 30, 17, 14, 12, 20, 13, 36, 68, 49, 37, 55, 56, 26];
 	Hint: What would the conditional for your for-loop look like?
 */
 
+
+for(var i = younger.length - 1; i >= 0; i--) {
+	console.log(younger[i]);
+}
+
+// or
+
+var younger = younger.reverse();
+
+console.log(younger);
+
 /*
 	Part 4: Now that you've finished the first 3 parts using a for-loop, repeat the same exercises
 	(Part 1, Part 2, and Part 3) using a while-loop.
 */
+
+// PART 1
+
+var ages = [28, 30, 17, 14, 12, 20, 13, 36, 68, 49, 37, 55, 56, 26];
+
+var i = 0;
+while(i < ages.length) {
+	console.log(ages[i]);
+	i++;
+}
+
+// PART 2
+
+var younger = [];
+i = 0; 
+while(i < ages.length) {
+	if(ages[i] <=20) {
+	younger.push(ages[i]);
+	}
+	i++;
+}
+
+var younger = [];
+i = 0; 
+while(i < ages.length) {
+	if(ages[i] <=20) {
+	younger.push(ages[i]);
+	}
+	i++;
+}
+
+console.log(younger.reverse());
+
 
 /*
 	Part 5: Create a function, getDayOfWeek, that will tell you what day of the week it is.
@@ -41,6 +99,29 @@ var ages = [28, 30, 17, 14, 12, 20, 13, 36, 68, 49, 37, 55, 56, 26];
 	Hint: Using a "switch" statement might result in cleaner code
 */
 
+function getDayOfWeek(number) {
+	switch(number) {
+		case 1:
+			return 'Monday';
+		case 2:
+			return 'Tuesday';
+		case 3:
+			return 'Wednesday';
+		case 4:
+			return 'Thursday';
+		case 5:
+			return 'Friday';
+		case 6:
+			return 'Saturday';
+		case 7:
+			return 'Sunday';
+		default:
+			return 'Please enter a valid day of the week';
+	}
+}
+
+console.log(getDayOfWeek(7));
+console.log(getDayOfWeek(100));
 /*
 	Bonus - Part 6: All this work is making me hungry. Let's create a new function, addPepperoniToPizza,
 	that accepts an array of pizza ingredients. If the array of ingredients already contains
